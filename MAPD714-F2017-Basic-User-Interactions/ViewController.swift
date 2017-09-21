a1ace213f41264375603465b6b0d8279ac496985
Student <student@setass-mac-mini-4.local>
2017-09-20 20:36:18 -0400

    Added action, outlet & action

--- MAPD714-F2017-Basic-User-Interactions/ViewController.swift
+++ MAPD714-F2017-Basic-User-Interactions/ViewController.swift
@@ -1,25 +1,40 @@
-//
-//  ViewController.swift
-//  MAPD714-F2017-Basic-User-Interactions
-//
-//  Created by Student on 2017-09-20.
-//  Copyright © 2017 Centennial College. All rights reserved.
-//
+/*
+ Name : Santhosh Damodharan
+ Date : Spetember 20, 2017
+ Student ID : 300964037
+ Description : Intermediate User Interaction Demo
+ Version : 0.1 - Built Basic UI & added outlet, action
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // OUTLETS ++++++++++++++++++++++++++++++++++++++++++
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    
+    // INHERTED METHODS +++++++++++++++++++++++++++++++++++++++++
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       }
+
+   // ACTION METHODS ++++++++++++++++++++++++++++++++++++++++++++++++
+
+    @IBAction func onSliderChanged(_ sender: UISlider) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onSegmentedControl(_ sender: UISegmentedControl) {
     }
-
-
+    
+    @IBAction func onDoSomethingPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+    }
+    
 }
 
